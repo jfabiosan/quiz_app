4a9cd8dd3b3bbeb271045ced20f2464ecc48fd33
Fabio <josefabiosan@gmail.com>
2023-06-29 22:49:24 -0300

    quiz app

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './result.dart';
 import './question.dart';
 import './question_box.dart';
 
@@ -31,8 +32,8 @@ class _HomeState extends State<Home> {
     },
     {
       3: [
-        Question('Dart é uima linguagem desenvolvida pelo Facebook', 0),
-        Question('Dart é uima linguagem desenvolvida pelo Google', 1),
+        Question('Dart é uma linguagem desenvolvida pelo Facebook', 0),
+        Question('Dart é uma linguagem desenvolvida pelo Google', 1),
       ]
     },
   ];
@@ -46,15 +47,41 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void reset() {
+    setState(() {
+      _index = 0;
+      result = 0;
+    });
+  }
+
+  String getMessage() {
+    if (result < 2) {
+      return 'Ooops... estude mais!';
+    } else if (result < 3) {
+      return 'Quase acertou tudo';
+    }
+    return 'Parabens... acertou tudo';
+  }
+
+  String _getTitle() {
+    if (_index < 4) {
+      return 'Question ${_index + 1}';
+    } else {
+      return 'Result';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Questions'),
+        title: Center(
+          child: Text(_getTitle()),
+        ),
       ),
       body: _index < _questions.length
           ? QuestionBox(_index, _questions, nextQuestion)
-          : const Text('Acabou'),
+          : Result(getMessage(), reset),
     );
   }
 }
---
+++ lib/result.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final String message;
+  final Function reset;
+
+  const Result(this.message, this.reset, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                message,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => reset(),
+              child: const Text('voltar'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
